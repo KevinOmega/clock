5b734e841a5c40e2560c232b01befcfba4e00207
Kevin Huayllas <KevinOmega01@gmail.com>
2022-02-23 20:14:51 -0400

    version 1.0

--- src/Timer.js
+++ src/Timer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useGLobalContext } from "./context";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { FaStopCircle } from "react-icons/fa";
@@ -18,44 +18,86 @@ const Timer = () => {
     setTime,
   } = useGLobalContext();
 
+  const [beep, setBeep] = useState(false);
+
   const handleReset = () => {
+    setIsRunning(false);
     setSeason(25);
     setRest(5);
     setTime({ minutes: season, seconds: 0 });
   };
 
-  const myTimer = () => {
-    const countDown =
-      new Date().getTime() + (time.minutes * 60000 + time.seconds * 6000);
-    const myInterval = setInterval(() => {
-      const now = new Date().getTime();
-      const timeleft = countDown - now;
-      var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-      setTime({ minutes, seconds });
-    }, 1000);
-    return () => clearInterval(myInterval);
-  };
+  useEffect(() => {
+    let timeOut = null;
+    if (beep && isRunning) {
+      let sound = document.getElementById("beep");
+      sound.currentTime = 0;
+      sound.volume = 75 / 100;
+      sound.play();
+      sound.setAttribute("autoplay", "autoplay");
+      timeOut = setTimeout(() => setBeep(false), 9000);
+    }
+
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, [beep, isRunning]);
 
   useEffect(() => {
-    if (isRunning) {
-      myTimer();
+    setCurrentType(true);
+    let countDown =
+      new Date().getTime() +
+      (time.minutes * 60000 + time.seconds * 1000) +
+      1000;
+    const myInterval = setInterval(() => {
+      let now = new Date().getTime();
+      let timeLeft = countDown - now;
+      let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+      setTime({ minutes, seconds });
+
+      if (minutes === 0 && seconds < 5) {
+        setBeep(true);
+      }
+
+      if (minutes <= 0 && seconds <= 0) {
+        setCurrentType(!currentType);
+        let newTime = 0;
+        if (currentType) {
+          newTime = rest;
+        } else {
+          newTime = season;
+        }
+        countDown = new Date().getTime() + newTime * 60000;
+      }
+    }, 1000);
+
+    if (!isRunning) {
+      clearInterval(myInterval);
+      countDown = 0;
     }
+    return () => {
+      clearInterval(myInterval);
+    };
   }, [isRunning]);
 
   return (
     <div className="timer-container">
       <div className="timer">
-        <p>{currentType ? "Session" : "Break"}</p>
-        <h4>
+        <p id="timer-label">{currentType ? "Session" : "Break"}</p>
+        <h4 id="time-left" className={time.minutes === 0 && "danger"}>
           {time.minutes}:{time.seconds}
         </h4>
+        <audio
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+          id="beep"
+        ></audio>
       </div>
       <div className="timer-btn">
-        <button onClick={() => setIsRunning(!isRunning)}>
+        <button id="start_stop" onClick={() => setIsRunning(!isRunning)}>
           {isRunning ? <FaStopCircle /> : <AiFillPlayCircle />}
         </button>
-        <button onClick={handleReset}>
+        <button id="reset" onClick={handleReset}>
           <GrPowerReset />
         </button>
       </div>
